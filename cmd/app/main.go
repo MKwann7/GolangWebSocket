fb5066873646294f32444dacf7c3b92b1edb188e
Micah Zak <micah@zakgraphix.com>
2021-04-10 11:56:13 -0500

    Abstracted configuration values.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,20 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strconv"
 )
 
 func main() {
+
+	// configuration
+	network := "tcp"
+	port := 8080
+
 	// listen on port 8080
-	ln, _ := net.Listen("tcp", ":8080")
+	ln, _ := net.Listen(network, ":"+strconv.Itoa(port))
 
 	// accept connection
 	conn, _ := ln.Accept()
 
 	// run loop forever (or until ctrl-c)
 	for {
-		// process data
-		// get message, output
 		message, _ := bufio.NewReader(conn).ReadString('\n')
+
 		fmt.Print("Message Received:", string(message))
 	}
 }
