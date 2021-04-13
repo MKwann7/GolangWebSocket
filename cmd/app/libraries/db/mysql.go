e30c142cdc40c9649d00d18b1e4b4b6e247b214c
Micah Zak <micah@zakgraphix.com>
2021-04-13 14:29:14 -0500

    Removed copious use of reflection and brought in sqlx package.m

--- cmd/app/libraries/db/mysql.go
+++ cmd/app/libraries/db/mysql.go
@@ -3,13 +3,14 @@ package db
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"log"
 	"reflect"
 )
 
 func GetWhere(connection Connection, model reflect.Type, whereClause string, sort string, limit int) ([]interface{}, error) {
 
-	database, databaseError := sql.Open("mysql", connection.UserName+":"+connection.Password+"@tcp("+connection.IpAddress+":"+connection.Port+")/"+connection.Database)
+	database, databaseError := sqlx.Open("mysql", connection.UserName+":"+connection.Password+"@tcp("+connection.IpAddress+":"+connection.Port+")/"+connection.Database)
 
 	if databaseError != nil {
 		return nil, databaseError
@@ -17,7 +18,7 @@ func GetWhere(connection Connection, model reflect.Type, whereClause string, sor
 
 	defer database.Close()
 
-	rows, queryError := database.Query("SELECT * FROM " + connection.Table + " WHERE " + whereClause)
+	rows, queryError := database.Queryx("SELECT * FROM " + connection.Table + " WHERE " + whereClause)
 
 	// if there is an error inserting, handle it
 	if queryError != nil {
@@ -31,16 +32,7 @@ func GetWhere(connection Connection, model reflect.Type, whereClause string, sor
 
 	for rows.Next() {
 		modelInstance := reflect.New(model)
-		modelElements := reflect.ValueOf(&modelInstance).Elem()
-		numCols := modelElements.NumField()
-		columns := make([]interface{}, numCols)
-
-		for i := 0; i < numCols; i++ {
-			field := modelElements.Field(i)
-			columns[i] = field.Addr().Interface()
-		}
-
-		err := rows.Scan(columns...)
+		err := rows.StructScan(modelInstance)
 		if err != nil {
 			log.Fatal(err)
 		}
